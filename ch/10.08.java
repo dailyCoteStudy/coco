bb89de8832bf03aad7fe6dcce5883a951702db06
chhong <pnci1029@gmail.com>
2024-10-08 20:54:33 +0900

    10.08 commit

---
+++ ch/10.08.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * 3
+ * https://www.acmicpc.net/problem/1436
+ */
+public class Solve {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int value = scanner.nextInt();
+
+
+        String result = "666";
+
+
+        while (true) {
+            if (result.contains("666")) {
+                value--;
+            }
+            if (value == 0) {
+                break;
+            } else {
+                result = String.valueOf(Integer.parseInt(result) + 1);
+            }
+        }
+        System.out.println(result);
+    }
+}
