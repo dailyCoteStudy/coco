e3fcaa502e69cdaf63a8369d106c92e0d1bfd255
chhong <pnci1029@gmail.com>
2024-10-10 20:26:38 +0900

    10.08 commit

--- ch/10.08.java
+++ ch/10.08.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
 /**
- * 3
- * https://www.acmicpc.net/problem/1436
+ 3
+ https://www.acmicpc.net/problem/1436
+
+ 숌
+
+ 1 = 666
+ 2 = 1666
+ 3 = 2...
+ 4 = 3
+ ..
+ 7= 6666
+ 8 = 7666
+ 
  */
 public class Solve {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        int value = scanner.nextInt();
+        int value = scanner.nextInt(); // 6
 
 
         String result = "666";
@@ -18,7 +29,9 @@ public class Solve {
         while (true) {
             if (result.contains("666")) {
                 value--;
+                System.out.println("result = " + result);
             }
+            System.out.println(value);
             if (value == 0) {
                 break;
             } else {
